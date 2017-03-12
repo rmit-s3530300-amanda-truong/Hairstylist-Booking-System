985044bae0a2c12d7c9456904739967647b650c0
jess_ <jess_@LAPTOP-36B6ABVT>
2017-03-12 22:10:32 +1100

    Mockup Test for Login

--- Team8/src/MenuTest.java
+++ Team8/src/MenuTest.java
@@ -10,4 +10,13 @@ public class MenuTest {
 		String[] expectedArray = {"cust0002", "john", "snow"};
 		assertEquals(expectedArray, actualArray);
 	}
+	
+	@Test
+	public void TestCustomerLogin() {
+		String password = "aaa";
+		String username = "john";
+		Boolean actualResult = m1.customerLogin();
+		Boolean expectedResult = true;
+		assertEquals(expectedResult, actualResult);
+	}
 }
