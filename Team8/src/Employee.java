493e2656903cc6f9080fcdb67eff230bc1fa6673
Jess <s3529550@student.rmit.edu.au>
2017-03-17 12:40:46 +1100

    Added in Skeleton of Files and also filled in some functions. Please look at TODOs.

---
+++ Team8/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee extends Role {
+	public enum Service {
+		femaleCut,
+		maleCut,
+		femaleDye,
+		maleDye,
+		femalePerm,
+		malePerm,
+		femaleWash,
+		maleWash
+	}
+	
+	private Service[] serviceType;
+	
+	public Employee(String ID, String firstName, String lastName, Service[] serviceType){
+		super(ID, firstName, lastName, null);
+		this.serviceType = serviceType.clone(); 
+	}
+	
+	// TODO: Needs Testing
+	public Service[] getService() {
+		return serviceType;
+	}
+}
