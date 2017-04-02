1f6521298e3b635f07334f494fa8b74711c83ab0
Jess <s3529550@student.rmit.edu.au>
2017-04-02 13:58:13 +1000

    Added in DisplayCalendar() works for current week, previous week and future week

--- Team8/src/Calendar/Calendar.java
+++ Team8/src/Calendar/Calendar.java
@@ -6,6 +6,7 @@ import java.util.*;
 import java.util.Map.Entry;
 
 import AppoinmentProgram.Booking;
+import Calendar.Calendar;
 
 public class Calendar {
 	public enum Status {
@@ -13,20 +14,42 @@ public class Calendar {
 		booked,
 		free
 	}
-	private HashMap<LocalDate,HashMap<LocalTime,Status>> information;
+	private LinkedHashMap<LocalDate,LinkedHashMap<LocalTime,Status>> information;
 	private Booking[] bookingPendingList;
 	private LocalDate currentDate;
 	private HashMap<String, Booking> bookingList;
 	
-	public Calendar(LocalDate date, HashMap<LocalDate,HashMap<LocalTime,Status>> info) {
-		information = info;
-		bookingList = new HashMap<String, Booking>();
+	public Calendar(LocalDate date) {
+		information = new LinkedHashMap<LocalDate, LinkedHashMap<LocalTime, Status>>();
 		currentDate = date;
-	}
-	
-	// TODO: Needs Testing
-	public void addAvailableTime(String employeeID, String date, String time) {
+		bookingList = new HashMap<String, Booking>();
 		
+		// Constructing calendar with a previous week of free time slots
+		LinkedHashMap<LocalTime,Calendar.Status> nested_info = new LinkedHashMap<LocalTime, Calendar.Status>();
+		LocalDate lastWeekDate = date.minusDays(7);
+		for(int x = 0;x<7;x++){
+			for(int i = 8; i<17 ;i++) {
+				LocalTime localtime = LocalTime.of(i, 00);
+				for(int y = 0 ; y<4 ;y++){
+					nested_info.put(localtime, Calendar.Status.free);
+					localtime = localtime.plusMinutes(15);
+				}
+				information.put(lastWeekDate, nested_info);	
+			}
+			lastWeekDate = lastWeekDate.plusDays(1);
+		}
+		// Constructing calendar with 2 weeks of free time slots
+		for(int x = 0;x<14;x++){
+			for(int i = 8; i<17 ;i++) {
+				LocalTime localtime = LocalTime.of(i, 00);
+				for(int y = 0 ; y<4 ;y++){
+					nested_info.put(localtime, Calendar.Status.free);
+					localtime = localtime.plusMinutes(15);
+				}
+				information.put(date, nested_info);	
+			}
+			date = date.plusDays(1);
+		}
 	}
 	
 	// TODO: Needs Testing
@@ -48,7 +71,11 @@ public class Calendar {
 		currentDate = date;
 	}
 	
-	public HashMap<LocalDate, HashMap<LocalTime, Status>> getCalendarInfo() {
+	public void setCalendarInfo(LinkedHashMap<LocalDate, LinkedHashMap<LocalTime, Status>> info) {
+		information = info;
+	}
+	
+	public LinkedHashMap<LocalDate, LinkedHashMap<LocalTime, Status>> getCalendarInfo() {
 		return information;
 	}
 	
@@ -58,25 +85,22 @@ public class Calendar {
 		information.get(date).put(time, Status.pending);
 	}
 	
-	// TODO: Have to do error checking in case there are no last 7 days in Calendar
-	// TODO: Needs Testing
-	public HashMap<LocalDate,HashMap<LocalTime,Status>> getHistory() {
+	public LinkedHashMap<LocalDate,LinkedHashMap<LocalTime,Status>> getHistory() {
 		LocalDate oldDate = currentDate.minusDays(7);
-		HashMap<LocalDate,HashMap<LocalTime,Status>> historyInfo = new HashMap<LocalDate, HashMap<LocalTime, Status>>();
+		LinkedHashMap<LocalDate,LinkedHashMap<LocalTime,Status>> historyInfo = new LinkedHashMap<LocalDate, LinkedHashMap<LocalTime, Status>>();
 		for(int i =0; i < 7; i++){
-			HashMap<LocalTime, Status> timeInfo = information.get(oldDate);
+			LinkedHashMap<LocalTime, Status> timeInfo = information.get(oldDate);
 			historyInfo.put(oldDate, timeInfo);
 			oldDate = oldDate.plusDays(1);
 		}
 		return historyInfo;
 	}
 	
-	// TODO: Needs Testing
-	public HashMap<LocalDate, HashMap<LocalTime, Status>> getNextWeek() {
+	public LinkedHashMap<LocalDate, LinkedHashMap<LocalTime, Status>> getNextWeek() {
 		LocalDate newDate = currentDate.plusDays(1);
-		HashMap<LocalDate,HashMap<LocalTime,Status>> futureInfo = new HashMap<LocalDate, HashMap<LocalTime, Status>>();
+		LinkedHashMap<LocalDate,LinkedHashMap<LocalTime,Status>> futureInfo = new LinkedHashMap<LocalDate, LinkedHashMap<LocalTime, Status>>();
 		for(int i =0; i < 7; i++){
-			HashMap<LocalTime, Status> timeInfo = information.get(newDate);
+			LinkedHashMap<LocalTime, Status> timeInfo = information.get(newDate);
 			futureInfo.put(newDate, timeInfo);
 		}
 		return futureInfo;
@@ -87,9 +111,52 @@ public class Calendar {
 		return bookingPendingList;
 	}
 	
-	// TODO: returns a concatenated String which displays the calendar on console
-	public String displayCalendar() {
-		return null;
+	// Returns a concatenated String which displays the calendar on console
+	public String displayCalendar(LinkedHashMap<LocalDate, LinkedHashMap<LocalTime, Status>> info, LocalDate startDate) {
+		String output;
+		LocalDate month_ = info.entrySet().iterator().next().getKey();
+		String month = month_.getMonth().toString();
+		output = String.format("%s\n", printBorder("-", 12*9+8));
+		output = output + String.format("%55s\n",month);
+		output = output + String.format("%s\n", printBorder("-", 12*9+8));
+		output = output + String.format("|%-13s", "");
+		LocalDate date = startDate; 
+		for(int i = 0; i < 7; i++){
+			output = output + String.format("|%-13s", date.toString());
+			date = date.plusDays(1);
+		}
+		output = output + String.format("\n%s\n", printBorder("-", 12*9+8));
+		LocalTime time = LocalTime.of(8, 00);
+		LocalDate date2=startDate;
+		while(!time.toString().equals(LocalTime.of(16, 15).toString())) {
+			date2 = startDate;
+			output = output + String.format("|%-13s| ", time.toString());
+			for(int i =0; i < 7; i ++) {
+				output = output + String.format("%-13s|", info.get(date2).get(time).toString());
+				date2.plusDays(1);
+			}
+			output = output + String.format("\n%s\n", printBorder("-", 12*9+8));
+			time = time.plusMinutes(15);
+		}
+		System.out.println(output);
+		return output;
 	}
 	
+	public String printBorder(String a, int times) {
+		String output="";
+		for(int i =0; i < times ; i++) {
+			output = output+a;
+		}
+		return output;
+	}
+	
+	public void printHashMap(LinkedHashMap<LocalDate, LinkedHashMap<LocalTime, Status>> information2) {
+		for(Entry<LocalDate, LinkedHashMap<LocalTime, Status>> entry : information2.entrySet()) {
+			LinkedHashMap<LocalTime, Status> entry2 = entry.getValue();
+			System.out.println("OuterKey: "+entry.getKey());
+			for(Entry<LocalTime, Status> entry3 : entry2.entrySet()) {
+				System.out.println("AKey: " + entry3.getKey() + " Value: " + entry3.getValue());
+			}			
+		}
+	}
 }
