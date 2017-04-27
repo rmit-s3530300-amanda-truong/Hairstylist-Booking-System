1b9e1765d4fa9d759fe7d868879ffd022d493338
Japan Patel <japanpatel1198@gmail.com>
2017-04-28 07:22:16 +1000

    Add employee minor UI changes.

--- Team8/src/application/CustomerPController.java
+++ Team8/src/application/CustomerPController.java
@@ -11,7 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
-public class CustomerPController{
+public class CustomerPController {
 	
 	private Menu menu;
 	
